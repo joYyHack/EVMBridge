c9270a0ed74dee4b49d99a84462d0fd6dd240433
joYyHack <agalaktionov5490@gmail.com>
2023-03-05 22:37:19 +0200

    Rename test erc20

--- contracts/test/ERC20.sol
+++ contracts/test/ERC20.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract TestERC20 is ERC20("Test", "TST") {
+contract SourceERC20 is ERC20("Source", "SRC") {
     function mint(uint256 _amount) public {
         _mint(msg.sender, _amount);
     }
