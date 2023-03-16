a43af525efd66b6447b70ab30c711c53a6ea2865
joYyHack <agalaktionov5490@gmail.com>
2023-03-16 03:18:45 +0200

    Add random erc20

---
+++ contracts/test/ERC20_Random.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier:MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract RandomERC20 is ERC20 {
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC20(_name, _symbol) {}
+
+    function mint(uint256 _amount) public {
+        _mint(msg.sender, _amount);
+    }
+}
