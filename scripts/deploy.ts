38dcd14dc272ac7e6e5c949665236878da590065
joYyHack <agalaktionov5490@gmail.com>
2023-03-16 03:18:06 +0200

    Add deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,58 @@
 import { ethers } from "hardhat";
+import { constants } from "ethers";
+import { parseEther, formatEther } from "ethers/lib/utils";
+import { privKey } from "./utils/encoding";
+import { faucet } from "./utils/faucet";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const provider = ethers.provider;
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  const bridgeOwner = new ethers.Wallet(privKey("666"), provider);
+  const alice = new ethers.Wallet(privKey("a11ce"), provider);
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  for (const wallet of [bridgeOwner, alice]) {
+    await faucet(wallet.address, provider);
+  }
 
-  await lock.deployed();
+  const bridgeFactory = await ethers.getContractFactory("Bridge", bridgeOwner);
+  const bridge = await bridgeFactory.deploy();
+  await bridge.deployed();
 
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  const erc20SafeHandlerFactory = await ethers.getContractFactory(
+    "ERC20SafeHandler",
+    bridgeOwner
+  );
+  const erc20Safe = await erc20SafeHandlerFactory.deploy(bridge.address);
+  await erc20Safe.deployed();
+
+  const validatorFactory = await ethers.getContractFactory(
+    "Validator",
+    bridgeOwner
+  );
+  const validator = await validatorFactory.deploy();
+  await validator.deployed();
+
+  await bridge.setERC20SafeHandler(erc20Safe.address);
+  await bridge.setValidator(validator.address);
+
+  const erc20Factory = await ethers.getContractFactory("SourceERC20", alice);
+  const ERC20 = await erc20Factory.deploy();
+  await ERC20.deployed();
+
+  await ERC20.mint(parseEther((100).toString()));
+  await ERC20.approve(erc20Safe.address, constants.MaxUint256);
+
+  console.log(`Bridge deployed at: ${bridge.address}`);
+  console.log(`ERC20 Safe deployed at: ${erc20Safe.address}`);
+  console.log(`Validator deployed at: ${validator.address}`);
+  console.log(
+    `ERC20 ${await ERC20.symbol()} token deployed at: ${ERC20.address}`
+  );
+  console.log(
+    `Alice's balance: ${formatEther(
+      await ERC20.balanceOf(alice.address)
+    ).toString()} of ${await ERC20.symbol()} tokens`
+  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
